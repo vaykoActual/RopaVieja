ea48cf6c4101a386cdc51a2c2fe1d722827ff9c3
vaykoactual <VAYKO@Shaynes-MacBook-Pro.local>
2021-03-07 16:51:49 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :categories, only: :index
+  resources :products
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :users, only: [:create, :index]
+  put '/categories/:category_id/products/:id', to: 'categories_products#add_category_to_product'
 end
